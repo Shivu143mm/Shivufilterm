797f377a9e1e21bdd58df27c4610b61d69f8e328
Shivu143mm <132443261+Shivu143mm@users.noreply.github.com>
2023-05-08 02:01:14 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Joelkb/DQ-the-file-donor.git /DQ-The-File-Donor
+  git clone https://github.com/Shivu143mm/Shivufilterm.git /Shivufilterm
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /DQ-The-File-Donor
+  git clone $UPSTREAM_REPO /Shivufilterm
 fi
-cd /DQ-The-File-Donor
+cd /Shivufilterm
 pip3 install -U -r requirements.txt
 echo "Starting DQ-The-File-Donor...."
 python3 bot.py
